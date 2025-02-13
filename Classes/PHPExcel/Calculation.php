24284e0a827edf3a50b8bf2cd32609d24cec492b
DisWEBru <romashkov.studio@gmail.com>
2025-02-14 01:53:17 +0500

    fix: convert memory_limit to bytes in public function privOptionDefaultThreshold, added isset to ['value']

--- Classes/PHPExcel/Calculation.php
+++ Classes/PHPExcel/Calculation.php
@@ -3346,7 +3346,7 @@ class PHPExcel_Calculation
 
                     //    If the last entry on the stack was a : operator, then we have a cell range reference
                     $testPrevOp = $stack->last(1);
-                    if ($testPrevOp['value'] == ':') {
+                    if (isset($testPrevOp['value']) && $testPrevOp['value'] == ':') {
                         //    If we have a worksheet reference, then we're playing with a 3D reference
                         if ($matches[2] == '') {
                             //    Otherwise, we 'inherit' the worksheet reference from the start cell reference
--- Classes/PHPExcel/Reader/Excel5.php
+++ Classes/PHPExcel/Reader/Excel5.php
@@ -1623,7 +1623,8 @@ class PHPExcel_Reader_Excel5 extends PHPExcel_Reader_Abstract implements PHPExce
                 //        max 2048 bytes will probably throw a wobbly.
                 $row = self::getInt2d($recordData, 0);
                 $extension = true;
-                $cellAddress = array_pop(array_keys($this->phpSheet->getComments()));
+                $array = array_keys($this->phpSheet->getComments());
+                $cellAddress = array_pop($array);
             }
 //            echo 'Note Address=', $cellAddress,'<br />';
 
@@ -5175,9 +5176,9 @@ class PHPExcel_Reader_Excel5 extends PHPExcel_Reader_Abstract implements PHPExce
                 }
                 //imagepng($ih, 'image.png');
 
-                $drawing = new PHPExcel_Worksheet_Drawing();
-                $drawing->setPath($filename);
-                $drawing->setWorksheet($this->phpSheet);
+                //$drawing = new PHPExcel_Worksheet_Drawing();
+                //$drawing->setPath($filename);
+                //$drawing->setWorksheet($this->phpSheet);
                 break;
             case 0x02: // Windows metafile or Macintosh PICT format
             case 0x0e: // native format
