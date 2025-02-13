10475a57a00d8c1e9a13a33af2fd24d8793a5cb2
Félix Labrecque <flabrecque@globallingua.ca>
2025-02-13 10:25:28 -0500

    Fix formula detection and formula writing to excel 2007 file

--- Classes/PHPExcel/Writer/Excel2007/Worksheet.php
+++ Classes/PHPExcel/Writer/Excel2007/Worksheet.php
@@ -768,7 +768,7 @@ class PHPExcel_Writer_Excel2007_Worksheet extends PHPExcel_Writer_Excel2007_Writ
             $objWriter->writeAttribute('ref', str_replace('$', '', $range));
 
             $columns = $pSheet->getAutoFilter()->getColumns();
-            if (count($columns > 0)) {
+            if (count($columns) > 0) {
                 foreach ($columns as $columnID => $column) {
                     $rules = $column->getRules();
                     if (count($rules) > 0) {
@@ -1114,7 +1114,7 @@ class PHPExcel_Writer_Excel2007_Worksheet extends PHPExcel_Writer_Excel2007_Writ
                         break;
                     case 'f':            // Formula
                         $attributes = $pCell->getFormulaAttributes();
-                        if ($attributes['t'] == 'array') {
+                        if (is_array($attributes) && array_key_exists('t', $attributes) && $attributes['t'] == 'array') {
                             $objWriter->startElement('f');
                             $objWriter->writeAttribute('t', 'array');
                             $objWriter->writeAttribute('ref', $pCellAddress);
